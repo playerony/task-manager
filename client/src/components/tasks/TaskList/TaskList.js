e912a3110418720e0ccc27f41a84c22b20303e28
playerony <pawel.wojtasinski.1995@gmail.com>
2018-09-29 07:34:39 +0200

    Create simple styles for task list page

--- client/src/components/tasks/TaskList/TaskList.js
+++ client/src/components/tasks/TaskList/TaskList.js
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 import requireAuth from "../../requireAuth";
 
+import "./TaskList.scss";
+
 class TaskList extends Component {
   render() {
-    return <div>TaskList</div>;
+    return (
+      <div className="task-list-wrapper">
+        <div className="task-list-mask">TaskList</div>
+        <Link to="/tasks/new" className="task-list--add-button">
+          +
+        </Link>
+      </div>
+    );
   }
 }
 
