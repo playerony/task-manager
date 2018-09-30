54e5f80a6e2015c8b1f88e0c399aee3b77a6f7f5
playerony <pawel.wojtasinski.1995@gmail.com>
2018-09-30 09:00:31 +0200

    Create more responsive styles for TaskCard component

--- client/src/components/tasks/TaskCard/TaskCard.js
+++ client/src/components/tasks/TaskCard/TaskCard.js
@@ -9,9 +9,9 @@ export default ({ _id, name, description, addedOn, onDelete }) => {
         <div className="task-card-delete" onClick={() => onDelete(_id)}>
           X
         </div>
-        <p>Added on: {new Date(addedOn).toLocaleDateString()}</p>
-        <h1>{name}</h1>
-        <p>{description}</p>
+        <div className="task-card-date">Added on: {new Date(addedOn).toLocaleDateString()}</div>
+        <div className="task-card-header">{name}</div>
+        <div className="task-card-description">{description}</div>
       </div>
     </div>
   );
