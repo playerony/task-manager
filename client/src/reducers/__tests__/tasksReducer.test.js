2a543e3fa8f1cc5f328a7aa82e26e763a4b667fb
playerony <pawel.wojtasinski.1995@gmail.com>
2018-09-30 15:41:03 +0200

    Create tests for tasks reducer

---
+++ client/src/reducers/__tests__/tasksReducer.test.js
@@ -0,0 +1,27 @@
+import tasksReducer from "../tasksReducer";
+import { FETCH_TASKS } from "../../actions/types";
+
+describe("tasks reducer", () => {
+  it("handles actions of type FETCH_ACTIONS", () => {
+    const action = {
+      type: FETCH_TASKS,
+      payload: [
+        {
+          _id: "123",
+          name: "test",
+          description: "test"
+        }
+      ]
+    };
+
+    const newState = tasksReducer([], action);
+
+    expect(newState).toEqual(action.payload);
+  });
+
+  it("handles action with unknown type", () => {
+    const newState = tasksReducer([], { type: "SOME_TEST" });
+
+    expect(newState).toEqual([]);
+  });
+});
